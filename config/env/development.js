7a463c4986047ef0a11e33d4586d77f9f23ea67d
anderles <anderles111@gmail.com>
2015-01-26 03:29:34 +0200

    test git - change title

--- config/env/development.js
+++ config/env/development.js
@@ -9,7 +9,7 @@ module.exports = {
     debug: false
   },
   app: {
-    name: 'MEAN - FullStack JS - Development'
+    name: 'MEAN - Web Video Platform - Development'
   },
   facebook: {
     clientID: 'DEFAULT_APP_ID',
--- config/env/production.js
+++ config/env/production.js
@@ -27,7 +27,7 @@ module.exports = {
     */
   },
   app: {
-    name: 'MEAN - A Modern Stack - Production'
+    name: 'MEAN - Web Video Platform - Production'
   },
   facebook: {
     clientID: 'APP_ID',
